18d96a4dedfa458f196629938f701e4b414ae33d
Ryan Field <fieldryan19@gmail.com>
2025-05-31 08:36:44 +0000

    completed calculateDeskLayout implementation

--- frontend/src/calculateDeskLayout.ts
+++ frontend/src/calculateDeskLayout.ts
@@ -1,7 +1,5 @@
-import { DogStatus, type PeopleQuery } from './generated/graphql';
-
-type Person = PeopleQuery['people'][0];
-const NO_TEAM_ID = 'none';
+import { DogStatus, type Person } from './generated/graphql';
+//changes this import to make the inline type hints more friendly to read
 
 /**
  * requirements teams must sit together.
@@ -28,28 +26,32 @@ const NO_TEAM_ID = 'none';
  * This test suite may not be exhaustive for all edge cases.
  */
 export const calculateDeskLayout = (people: Person[]): Person[] => {
-  if(people.length <= 2) return people
+  if (people.length <= 2) return people
 
   const teams = splitIntoTeams(people);
   const bestTeamLayouts = calculateBestLayouts(teams);
 
-  const bestLayout: Person[] = [];
+  /**
+   * To guarantee the best possible outcome I could have brute forced literally every single combination
+   * of teams against every combination of every other teams. As sort of a tradeoff I instead decided
+   * to find the best possible layout within each team and then find the best combination of all those layouts.
+   */
+  const bestTeamLayoutsPermutations = addFlippedTeams(bestTeamLayouts);
+  const possibleLayouts = generateLayouts(bestTeamLayoutsPermutations);
+  const bestPossibleLayout = findBestLayout(possibleLayouts);
 
-  bestTeamLayouts.forEach((value) => {
-    bestLayout.push(...value);
-  })
-
-  return bestLayout;
+  return bestPossibleLayout;
 };
 
-function splitIntoTeams(people: Person[]) : Map<string, Person[]> {
+function splitIntoTeams(people: Person[]): Map<string, Person[]> {
   const teams = new Map<string, Person[]>();
 
   people.forEach((person) => {
 
-    const teamId = person.team ? person.team.id : NO_TEAM_ID;        
+    //treat people without a team as a team of 1 instead of grouping them all together
+    const teamId = person.team ? person.team.id : JSON.stringify(person);
 
-    if(teams.has(teamId)) {
+    if (teams.has(teamId)) {
       teams.get(teamId)?.push(person);
     } else {
       teams.set(teamId, [person])
@@ -62,7 +64,6 @@ function calculateBestLayouts(teams: Map<string, Person[]>): Map<string, Person[
   const optimizedTeams = new Map<string, Person[]>();
 
   teams.keys().forEach(teamId => {
-    if(teamId === NO_TEAM_ID) optimizedTeams.set(teamId, teams.get(teamId)!) // we'll do something else for people who don't have a team later
     optimizedTeams.set(teamId, optimizeTeam(teams.get(teamId)!))
   })
 
@@ -71,33 +72,33 @@ function calculateBestLayouts(teams: Map<string, Person[]>): Map<string, Person[
 
 function optimizeTeam(people: Person[]): Person[] {
 
-  if(people.length <= 2) return people;
+  if (people.length <= 2) return people;
 
   let bestLayout: Person[] = [];
   let bestLayoutScore = Number.NEGATIVE_INFINITY;
 
-  const teamPermutations = generatePermutations(people);
+  const teamPermutations = generateTeamPermutations(people);
   teamPermutations.forEach(permutation => {
-    const score = calculateScore(permutation);
+    const score = calculateTeamScore(permutation);
 
     if (score > bestLayoutScore) {
-            bestLayout = permutation;
-            bestLayoutScore = score;
-        }
+      bestLayout = permutation;
+      bestLayoutScore = score;
+    }
   })
 
   return bestLayout;
 }
 
-function generatePermutations(allPeople: Person[]): Person[][] {
+function generateTeamPermutations(allPeople: Person[]): Person[][] {
   let permutations: Person[][] = [[]];
-  allPeople.forEach(( person ) => {
+  allPeople.forEach((person) => {
     const currentPermutation: Person[][] = [];
 
-    permutations.forEach( permutation => {
-      for(let i = 0; i <= permutation.length; i++) {
+    permutations.forEach(permutation => {
+      for (let i = 0; i <= permutation.length; i++) {
         const temp = [...permutation];
-        temp.splice(i , 0, person);
+        temp.splice(i, 0, person);
         currentPermutation.push(temp);
       }
     })
@@ -106,37 +107,37 @@ function generatePermutations(allPeople: Person[]): Person[][] {
   return permutations;
 }
 
-function calculateScore(people: Person[]): number {
+function calculateTeamScore(people: Person[]): number {
   let score = 0;
   let minAvoidDistance = null;
   let minHaveDistance = null;
 
-  for(let i = 0; i < people.length - 1; i++) {
+  for (let i = 0; i < people.length - 1; i++) {
 
-    for(let j = i + 1; j <= people.length - 1; j++) {
-        const distance = j - i;
+    for (let j = i + 1; j <= people.length - 1; j++) {
+      const distance = j - i;
 
-      if(oneAvoidOneHave(people[i], people[j])) {
+      if (oneAvoidOneHave(people[i], people[j])) {
         score -= people.length - distance;
         if (!minAvoidDistance) {
           minAvoidDistance = distance;
-        } else if(distance < minAvoidDistance) {
+        } else if (distance < minAvoidDistance) {
           minAvoidDistance = distance;
         }
-      } 
-      
+      }
+
       if (people[i].dogStatus === DogStatus.Have && people[j].dogStatus === DogStatus.Have) {
         score -= people.length - distance;
-        if(!minHaveDistance){
+        if (!minHaveDistance) {
           minHaveDistance = distance;
-        } else if ( distance < minHaveDistance) {
+        } else if (distance < minHaveDistance) {
           minHaveDistance = distance;
         }
       }
     }
   }
 
-  if(minHaveDistance && minAvoidDistance && (minAvoidDistance > minHaveDistance)) {
+  if (minHaveDistance && minAvoidDistance && (minAvoidDistance > minHaveDistance)) {
     const distanceDifference = minAvoidDistance - minHaveDistance;
     score -= distanceDifference;
   }
@@ -145,6 +146,81 @@ function calculateScore(people: Person[]): number {
 }
 
 function oneAvoidOneHave(p1: Person, p2: Person) {
- return (p1.dogStatus === DogStatus.Avoid && p2.dogStatus === DogStatus.Have)
- || (p1.dogStatus === DogStatus.Have && p2.dogStatus === DogStatus.Avoid)
+  return (p1.dogStatus === DogStatus.Avoid && p2.dogStatus === DogStatus.Have)
+    || (p1.dogStatus === DogStatus.Have && p2.dogStatus === DogStatus.Avoid)
 }
+
+function addFlippedTeams(teams: Map<string, Person[]>): Map<string, Person[][]> {
+  const inclFlippedTeams = new Map<string, Person[][]>();
+
+  teams.forEach((value, key) => {
+    if (value.length === 1) {
+      inclFlippedTeams.set(key, [value])
+
+    } else {
+      inclFlippedTeams.set(key, [[...value], value.reverse()])
+    }
+  })
+
+  return inclFlippedTeams;
+}
+
+function generateLayouts(allTeamLayouts: Map<string, Person[][]>): Person[][] {
+  let allLayouts: Person[][][] = [[[]]];
+
+  allTeamLayouts.forEach(teamLayouts => {
+
+    const incompleteLayouts = [...allLayouts];
+
+    const layoutsWithTeamLayouts: Person[][][] = [];
+    incompleteLayouts.forEach(layout => {
+      teamLayouts.forEach(teamLayout => {
+
+        for (let i = 0; i <= layout.length; i++) {
+          const temp = [...layout];
+          temp.splice(i, 0, teamLayout);
+          layoutsWithTeamLayouts.push(temp);
+        }
+      })
+      allLayouts = layoutsWithTeamLayouts;
+    })
+  })
+  return allLayouts.map(layout => layout.flat());
+}
+
+function findBestLayout(layouts: Person[][]): Person[] {
+  let bestLayout: Person[] = [];
+  let bestLayoutScore = Number.NEGATIVE_INFINITY;
+
+  layouts.forEach(permutation => {
+    const score = calculateLayoutScore(permutation);
+
+    if (score > bestLayoutScore) {
+      bestLayout = permutation;
+      bestLayoutScore = score;
+    }
+  })
+  return bestLayout;
+}
+
+function calculateLayoutScore(people: Person[]): number {
+  let score = 0;
+
+  for (let i = 0; i < people.length - 1; i++) {
+
+    for (let j = i + 1; j <= people.length - 1; j++) {
+      const distance = j - i;
+
+      if (oneAvoidOneHave(people[i], people[j])) {
+        score -= people.length - distance;
+      }
+
+      if (people[i].dogStatus === DogStatus.Have && people[j].dogStatus === DogStatus.Have) {
+        score -= people.length - distance;
+      }
+    }
+  }
+  return score;
+}
+
+
