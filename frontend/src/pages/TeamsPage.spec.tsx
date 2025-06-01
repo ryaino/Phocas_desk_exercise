7e312650cfb3e0fe20d0bbe4ec235634a83958d4
Ryan Field <fieldryan19@gmail.com>
2025-06-01 05:34:23 +0000

    added test for deleting team

--- frontend/src/pages/TeamsPage.spec.tsx
+++ frontend/src/pages/TeamsPage.spec.tsx
@@ -1,7 +1,8 @@
 import { MockedProvider } from '@apollo/client/testing';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
-import { TEAM_QUERY } from '../queries/teams';
+import { DELETE_TEAM, TEAM_QUERY } from '../queries/teams';
 import { mock } from '../testUtil';
 import TeamsPage from './TeamsPage';
 
@@ -15,15 +16,18 @@ const mocks = [
           id: '3',
           name: 'A team',
           members: [],
+          __typename: 'Team',
         },
         {
           id: '4',
           name: 'B team',
           members: [],
+          __typename: 'Team',
         },
       ],
     },
   ),
+  mock(DELETE_TEAM, { id: '3' }, { deleteTeam: { id: '3', __typename: 'Team' } }),
 ];
 
 describe('TeamsPage', () => {
@@ -38,5 +42,26 @@ describe('TeamsPage', () => {
     expect(names).toHaveLength(2);
     expect(names[0].querySelector('input')).toHaveValue('A team');
     expect(names[1].querySelector('input')).toHaveValue('B team');
+  });
+
+  it('test delete', async () => {
+    const container = render(
+      <MockedProvider mocks={mocks}>
+        <TeamsPage />
+      </MockedProvider>,
+    );
+
+    const deletes = await container.findAllByTestId('delete');
+    expect(deletes).toHaveLength(2);
+
+    const user = userEvent.setup();
+
+    await user.click(deletes[0]);
+
+    const names = await container.findAllByTestId('name');
+
+    expect(names).toHaveLength(1);
+
+    expect(names[0].querySelector('input')).toHaveValue('B team');
   });
 });
